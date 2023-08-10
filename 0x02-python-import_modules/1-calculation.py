53ded8cebdc32e9a9229991c6fd68ace6d70da0d
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-10 18:26:55 +0000

    change line count

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 import calculator_1 as ped
-
 a = 10
 b = 5
-
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, ped.add(a, b)))
     print("{} - {} = {}".format(a, b, ped.sub(a, b)))
