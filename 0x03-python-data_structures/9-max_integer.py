2128225d62d60584d8e2a10e0cea7f32ecf3a139
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-14 18:21:55 +0000

    max without max()-2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,11 @@
 
 
 def max_integer(my_list=[]):
-    biggest_num = 0
 
     if (len(my_list) == 0):
         return None
+
+    biggest_num = my_list[0]
     for num in my_list:
         if (num > biggest_num):
             biggest_num = num
