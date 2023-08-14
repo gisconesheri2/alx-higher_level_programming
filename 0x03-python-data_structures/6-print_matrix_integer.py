05e26ba28af24b74ff556911ce46def82cdd4f7e
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-14 17:21:31 +0000

    print list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,16 +2,14 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    i = 0;
+    i = 0
 
     for l1 in matrix:
         for num in l1:
-            #print("i is now {}".format(i))
             if (i == 2):
-                #print("printing end number")
                 print("{:d}".format(num))
             else:
-                print("{:d}".format(num), end =" ")
+                print("{:d}".format(num), end=" ")
             i = i + 1
         i = 0
     if (len(matrix[0]) == 0):
