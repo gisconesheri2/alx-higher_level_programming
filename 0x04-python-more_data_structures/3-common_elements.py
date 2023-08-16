d7ef7a5f70d7a84df1bbc88429237bf2fb59330c
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-16 20:46:15 +0000

    exercises 1 to 4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,7 @@
 
 
 def common_elements(set_1, set_2):
-    #return (set_1 & set_2)
+    """
+    return (set_1 & set_2)
+    """
     return (set_1.intersection(set_2))
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,7 @@
 
 
 def only_diff_elements(set_1, set_2):
-    #return (set_1 ^ set_2)
+    """
+    return (set_1 ^ set_2)
+    """
     return (set_1.symmetric_difference(set_2))
