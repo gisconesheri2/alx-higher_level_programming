b10c9dc46e3e9fa77226643f5c101e334c7b7183
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-29 20:09:08 +0000

    position argument

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Square module for everything
+"""
+
+
+class Square:
+    """ defines a square. """
+    def __init__(self, size=0, position=(0, 0)):
+        """ initialize the data
+        Args:
+            position (tuple(int, int)): offset to print square
+            size (int): dimensions of the square
+
+        Attributes:
+            position (tuple(int, int)): offset to print square
+            size: dimensions of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must >= 0")
+        self.__size = size
+        self.position = position
+
+    @property
+    def position(self):
+        """retrieve the value of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, val):
+        """sets the value of position offset
+        Args:
+            val (tuple(int, int)): offset for printing the square
+        """
+        if len(val) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(val[0]) != int or type(val[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = val
+
+    @property
+    def size(self):
+        """retrieve the value of size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set the value of size to value
+
+            Args:
+            value (int): value of size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates the area of the square
+        Returns:
+            int: area of the square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """print a square using '#'"""
+        if self.__size == 0:
+            print()
+        for ln in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for ln in range(self.__position[0]):
+                print(" ", end="")
+            for l in range(self.__size):
+                print("#", end="")
+            print()
