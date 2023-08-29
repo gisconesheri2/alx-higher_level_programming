ee4e6732d165977c4b6ff4b72731994d1d45b366
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-29 17:14:37 +0000

    class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """ defines a square. """
+
+    def __init__(self, size):
+        """ initialize the data
+        Args:
+            size (int): dimensions of the square
+        
+        Attributes:
+            size: dimensions of the square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """ defines a square. """
+
+    def __init__(self, size=0):
+        """ initialize the data
+        Args:
+            size (int): dimensions of the square
+        
+        Attributes:
+            size: dimensions of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must >= 0")
+        self.__size = size
