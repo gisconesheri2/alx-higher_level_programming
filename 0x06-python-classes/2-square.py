23c70706994ef52885a49b7abb7a5f32f0d0fbd1
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-08-31 14:37:20 +0000

    minor fixes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,5 +18,5 @@ class Square:
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
