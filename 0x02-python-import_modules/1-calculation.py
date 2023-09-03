bcbb42da61696fc32299b444e58d9152849d32c9
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-09-03 14:30:08 +0000

    fixed 1-calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as ped
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, ped.add(a, b)))
-    print("{} - {} = {}".format(a, b, ped.sub(a, b)))
-    print("{} * {} = {}".format(a, b, ped.mul(a, b)))
-    print("{} / {} = {}".format(a, b, ped.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
