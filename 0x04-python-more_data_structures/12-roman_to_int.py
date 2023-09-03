03528ebe7f0a4ef5d8850babd714a569e1639771
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-09-03 14:38:53 +0000

    fix roman string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,8 +7,8 @@ def roman_to_int(roman_string):
     roman_num["I"] = 1
     dec = 0
     last_letter = ""
-    
-    if type(roman_string) != str or roman_string == None:
+
+    if type(roman_string) != str or roman_string is None:
         return 0
 
     if len(roman_string) == 1:
