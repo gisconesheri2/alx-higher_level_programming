3908578bb50acc1d635830b4dcb69dc011a90005
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-09-13 06:37:40 +0000

    fises fises

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
 """contains a empty class BaseGeometry
 """
-
-
-class BaseGeometry(object):
-    """an empty class that inherits from object"""
-
-    def area(self):
-        """calculate area of a grometric shape"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an int greater than 0
-        Args:
-            name (str): identifier for the value
-            value (int):
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
