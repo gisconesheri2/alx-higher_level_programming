e879fc0e7ef5c138f0ddb3ddd14bf2cbd8078d14
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-17 20:35:07 +0000

    advance tasks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- display average temperatur by city
-SELECT state MAX(value) AS max_temp
+SELECT state, MAX(value) AS max_temp
 	FROM temperatures
 	GROUP BY state
-	ORDER BY state
+	ORDER BY state;
