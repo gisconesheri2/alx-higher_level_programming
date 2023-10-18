71d80d49817ecd6b46e3126c019097556961ddb1
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-18 18:04:40 +0000

    create secnd user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create a database and a user
+-- grant only read privileges
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
