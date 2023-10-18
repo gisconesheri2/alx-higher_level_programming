7c7f849bd081616d8c617f46f1d0fc232210c2f8
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-18 21:32:00 +0000

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@ SELECT tg.name
 FROM tv_show_genres AS tsg
 INNER JOIN tv_genres AS tg
 ON tg.id = tsg.genre_id
-WHERE tsg.show_id = 
+WHERE tsg.show_id = -- get id for dexter 
 	(SELECT id
 	 FROM tv_shows
 	 WHERE title = 'Dexter')
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- show tv_shows with at least one genre
 SELECT ts.title, tg.name
-FROM tv_genres as tg
+FROM tv_genres AS tg
 RIGHT JOIN
 	(tv_shows AS ts LEFT OUTER JOIN tv_show_genres AS tsg
 	ON ts.id = tsg.show_id)
