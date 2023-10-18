5f43b1154222bdd21f79d8e1a263bf99ac965659
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-18 17:56:46 +0000

    create a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create a user and grant all priviledeges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
