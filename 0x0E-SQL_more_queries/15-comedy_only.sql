a54fc35fe67f92cf886cb5a593e2aa2909253623
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-18 22:08:58 +0000

    fix 3

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,8 +3,7 @@ SELECT ts.title
 FROM tv_show_genres AS tsg
 INNER JOIN tv_shows AS ts
 ON ts.id = tsg.show_id
-WHERE tsg.genre_id = 
-	(SELECT id
-	 FROM tv_genres
-	 WHERE name = 'Comedy')
+JOIN tv_genres AS tg 
+ON tsg.genre_id = tg.id
+WHERE tg.name = 'Comedy'
 ORDER BY ts.title ASC;
