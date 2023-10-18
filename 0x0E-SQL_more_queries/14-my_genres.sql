265bdc4e0258a4d4d6c045eaffc7673dbefd0ee4
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-10-18 22:04:46 +0000

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- show genres for a specif show
-select t.name 
-from tv_genres as t
-join tv_show_genres AS tsg
-on t.id = tsg.genre_id
+SELECT t.name 
+FROM tv_genres AS t
+JOIN tv_show_genres AS tsg
+ON t.id = tsg.genre_id
 INNER JOIN tv_shows AS ts
 ON ts.id = tsg.show_id
 WHERE ts.title = 'Dexter'
-order by t.name
+ORDER BY t.name
