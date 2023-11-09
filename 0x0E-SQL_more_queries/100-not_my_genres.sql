a8e6ca817c725beb3cb07290d5e39399371f76ff
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-11-09 16:51:39 +0000

    left joinimplentation

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,12 @@
 -- show genres not linked to show 'Dexter'
 SELECT tg.name
-FROM tv_genres as tg
+FROM tv_genres AS tg
 LEFT JOIN 
 	(SELECT tsg.genre_id
 	FROM tv_show_genres AS tsg
 	INNER JOIN tv_shows AS ts
 	ON ts.id = tsg.show_id
-	WHERE ts.title = "Dexter") as dexter_genres (id)
+	WHERE ts.title = "Dexter") AS dexter_genres (id)
 ON tg.id = dexter_genres.id
-WHERE dexter_genres.id is NULL
+WHERE dexter_genres.id IS NULL
 ORDER BY tg.name ASC;
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,12 @@
--- show genres not linked to show 'Dexter'
+-- shows that are not comedies
 SELECT ts.title
-FROM tv_shows as ts
+FROM tv_shows AS ts
 LEFT JOIN 
 	(SELECT tsg.show_id
 	FROM tv_show_genres AS tsg
 	INNER JOIN tv_genres AS tg
 	ON tg.id = tsg.genre_id
-	WHERE tg.name = "Comedy") as comedy_shows (id)
+	WHERE tg.name = "Comedy") AS comedy_shows (id)
 ON ts.id = comedy_shows.id
-WHERE comedy_shows.id is NULL
+WHERE comedy_shows.id IS NULL
 ORDER BY ts.title ASC;
