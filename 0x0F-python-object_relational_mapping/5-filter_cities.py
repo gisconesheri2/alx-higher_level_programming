51542f3a88b6d64c38cab5b5209f3e268154c13f
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-11-18 18:55:12 +0000

    use only one execute

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,27 +9,27 @@ if __name__ == "__main__":
                              db=sys.argv[3], charset="utf8")
 
     cur = database.cursor()
-    cur2 = database.cursor()
-    cur2.execute("SELECT name FROM states")
     search_term = sys.argv[4]
+    stop = False
+    forbidden_chars = [';', 'from', '*', '=', 'select', 'where']
 
-    for name in cur2.fetchall():
-        if search_term == name[0]:
-            cur.execute("""SELECT cities.name
+    for char in forbidden_chars:
+        if char in search_term.lower():
+            stop = True
+
+    if stop is False:
+        cur.execute("""SELECT cities.name
                         FROM states
                         INNER JOIN cities
                         ON states.id = cities.state_id
                         WHERE states.name LIKE '%{}'
                         ORDER BY cities.id ASC""".format(sys.argv[4]))
 
-            query_rows = cur.fetchall()
-            if len(query_rows) > 0:
-                results = f'{query_rows[0][0]}'
-                for row in query_rows[1:]:
-                    results = results + f', {row[0]}'
-
-                print(results)
-            break
+        query_rows = cur.fetchall()
+        if len(query_rows) > 0:
+            results = f'{query_rows[0][0]}'
+        for row in query_rows[1:]:
+            results = results + f', {row[0]}'
+    print(results)
     cur.close()
-    cur2.close()
     database.close()
