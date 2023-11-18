d5bef4720b8985e76493c4cce508f91296e14019
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-11-18 20:10:14 +0000

    get city working

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Declare Base class and extend it to create
+the nodel table"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import City
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """template for the states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    def __init__(self, name):
+        """initiliaze the state properties"""
+        self.name = name
