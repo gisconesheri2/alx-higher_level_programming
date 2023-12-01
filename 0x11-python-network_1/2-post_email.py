25eee37dc664ebccb48aaac16b161eb33c27c56e
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-12-01 17:45:47 +0000

    fix post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,9 @@ import sys
 
 if __name__ == "__main__":
     email = sys.argv[2]
-    data = urllib.parse.urlencode(email)
-    data = data.encode('utf-8')
+    values = {'email' : email}
+    data = urllib.parse.urlencode(values)
+    data = email.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
 
     with urllib.request.urlopen(req) as resp:
