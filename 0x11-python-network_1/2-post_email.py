58c9554da300a5f16e51931718f0950695bd4834
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-12-01 17:29:58 +0000

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ import urllib.request
 import urllib.parse
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     email = sys.argv[2]
     data = urllib.parse.urlencode(email)
     data = data.encode('utf-8')
